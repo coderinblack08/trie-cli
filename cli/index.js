5fb707fe39d39ce906016c472acfbde891464051
coderinblack08 <kevinlu.email@gmail.com>
2022-07-24 00:25:39 -0700

    feat: complete rest of CLI

--- cli/index.js
+++ cli/index.js
@@ -18,11 +18,27 @@ program
 
 program
   .command("search <word>")
-  .description("Checks whether word is in the trie");
+  .description("Checks whether word is in the trie")
+  .action(async (word) => {
+    const searchResult = await axios.get(`${API_URL}/search?word=${word}`);
+    if (searchResult.data) {
+      console.log(chalk.bgGreen(`'${word}' exists within trie`));
+    } else {
+      console.log(chalk.bgRed(`'${word}' doesn't exist within trie`));
+    }
+  });
 
 program
   .command("starts-with <prefix>")
-  .description("Check whether any word in the trie contains the prefix");
+  .description("Check whether any word in the trie contains the prefix")
+  .action(async (prefix) => {
+    const startsWith = await axios.get(`${API_URL}/starts-with?word=${prefix}`);
+    if (startsWith.data) {
+      console.log(chalk.bgGreen(`Word starting with '${prefix}' exists`));
+    } else {
+      console.log(chalk.bgRed(`No word stars with '${prefix}'`));
+    }
+  });
 
 program
   .command("add <word>")
@@ -36,6 +52,16 @@ program
     }
   });
 
-program.command("delete <word>").description("Delete a word to the trie");
+program
+  .command("delete <word>")
+  .description("Delete a word to the trie")
+  .action(async (word) => {
+    try {
+      await axios.delete(API_URL, { word });
+      console.log(chalk.bgGreen(`Deleted ${word} successfully`));
+    } catch (error) {
+      console.log(chalk.bgRed(`Error occurred while deleting ${word}`));
+    }
+  });
 
 program.parse();
